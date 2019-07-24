d431d889931fb925e8660eeb96e3048a42a8417d
Augusto Xavier <augustocbx@gmail.com>
2019-07-24 19:48:00 -0300

    Aula Section 3 / 25 Implementação rede multicamada

--- rede_multicamada.py
+++ rede_multicamada.py
@@ -14,29 +14,17 @@ def sigmoid(soma):
 #a = sigmoid(1.5)
 #b = np.exp(0)
     
-entradas = np.array([
-        [0,0],
+entradas = np.array([[0,0],
         [0,1],
         [1,0],
-        [1,1]
-        ])
+        [1,1]])
     
-saidas = np.array([
-        [0],
-        [1],
-        [1],
-        [0],
-        ])
+saidas = np.array([[0],[1],[1],[0]])
         
-pesos0 = np.array([
-            [-0.424, -0.740, -0.961],
-            [0.358, -0.577, -0.469]
-        ])
+pesos0 = np.array([[-0.424, -0.740, -0.961],
+            [0.358, -0.577, -0.469]])
     
-pesos1 = np.array([[
-        -0.017, 
-        -0.893, 
-        -0.148]])
+pesos1 = np.array([[-0.017], [-0.893], [-0.148]])
     
 epocas = 100
 
@@ -44,3 +32,7 @@ for j in range(epocas):
     camadaEntrada = entradas
     somaSinapse0 = np.dot(camadaEntrada, pesos0)
     camadaOculta = sigmoid(somaSinapse0)
+    
+    somaSinapse1 = np.dot(camadaOculta, pesos1)
+    camadaSaida = sigmoid(somaSinapse1)
+    
